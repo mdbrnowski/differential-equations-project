4444641a3558ed805b72d3d81615c6c8cce46a92
Michał Dobranowski <mdbrnowski@gmail.com>
2023-12-18 23:39:12 +0100

    Fix code style in integration.jl

--- integration.jl
+++ integration.jl
@@ -1,23 +1,22 @@
 using LinearAlgebra
 
-function gauss_legendre_quadrature_on_standard_interval(f, n)
+function gauss_legendre_quadrature(f::Function, n::Int64)::Float64
     if n == 2
-        println(2)
-        xs = [-1/sqrt(3), 1/sqrt(3)]
+        xs = [-1 / sqrt(3), 1 / sqrt(3)]
         weights = [1, 1]
     elseif n == 3
-        println(3)
-        xs = [-sqrt(3/5), 0, sqrt(3/5)]
-        weights = [5/9, 8/9, 5/9]
+        xs = [-sqrt(3 / 5), 0, sqrt(3 / 5)]
+        weights = [5 / 9, 8 / 9, 5 / 9]
     else
         error("n should be equal to 2 or 3")
     end
-    result = dot(weights, map(f, xs))
+    result = dot(weights, f.(xs))
     return result
 end
 
-function gauss_legendre_quadrature(f, a, b)
+" Integrates f on range (a, b). Uses Gauss-Legendre quadrature with n = 3. "
+function integrate(f::Function, a::Float64, b::Float64)::Float64
     g(x) = f((x + 1) / 2 * (b - a) + a)
-    result = gauss_legendre_quadrature_on_standard_interval(g, 3)
+    result = gauss_legendre_quadrature(g, 3)
     return result * (b - a) / 2
 end
