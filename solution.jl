1f93eeba67a6d60f6234681009296615dcc4cc91
Michał Dobranowski <mdbrnowski@gmail.com>
2023-12-21 13:48:47 +0100

    Add alias for Phi and `@__FILE__` check

--- solution.jl
+++ solution.jl
@@ -15,7 +15,7 @@ const G::Real = 1
 x̂(i::Integer) = 3 * i / n
 
 function e(i::Integer, x::Real)::Real
-    if (x̂(i - 1) < x < x̂(i))
+    if x̂(i - 1) < x < x̂(i)
         return (x - x̂(i - 1)) / (3 / n)
     elseif (x̂(i) < x < x̂(i + 1))
         return (x̂(i + 1) - x) / (3 / n)
@@ -24,7 +24,7 @@ function e(i::Integer, x::Real)::Real
 end
 
 function e′(i::Integer, x::Real)::Real
-    if (x̂(i - 1) < x < x̂(i))
+    if x̂(i - 1) < x < x̂(i)
         return 1 / (3 / n)
     elseif (x̂(i) < x < x̂(i + 1))
         return -1 / (3 / n)
@@ -35,10 +35,10 @@ end
 " returns B(e_i, e_j) "
 function B(i::Integer, j::Integer)::Real
     i, j = minmax(i, j)
-    if (j == i)
+    if j == i
         return -(∫(x -> e′(i, x)^2, x̂(i - 1), x̂(i)) +
                  ∫(x -> e′(i, x)^2, x̂(i), x̂(i + 1)))
-    elseif (j == i + 1)
+    elseif j == i + 1
         return -(∫(x -> e′(i, x) * e′(j, x), x̂(i - 1), x̂(i)) +
                  ∫(x -> e′(i, x) * e′(j, x), x̂(i), x̂(j)) +
                  ∫(x -> e′(i, x) * e′(j, x), x̂(j), x̂(j + 1)))
@@ -51,7 +51,7 @@ end
 function L(i::Integer)::Real
     result = ∫(x -> Φ̃′(x) * e′(i, x), x̂(i - 1), x̂(i)) +
              ∫(x -> Φ̃′(x) * e′(i, x), x̂(i), x̂(i + 1))
-    if (x̂(i + 1) >= 1 && x̂(i - 1) <= 2)
+    if x̂(i + 1) >= 1 && x̂(i - 1) <= 2
         result += 4π * G * (∫(x -> e(i, x), x̂(i - 1), x̂(i)) +
                             ∫(x -> e(i, x), x̂(i), x̂(i + 1)))
     end
@@ -66,8 +66,12 @@ function Φ(x::Real)
     return dot(weights, [e(i, x) for i in 1:n-1]) + Φ̃(x)
 end
 
-x = range(0, 3, 1000)
-plot(x, Φ.(x), label=L"\Phi", lc=:black, lw=2, legend=false, xguidefontsize=14, yguidefontsize=14)
-xlabel!(L"x")
-ylabel!(L"\Phi(x)")
-savefig("plot.pdf")
+const Phi = Φ
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    x = range(0, 3, 1000)
+    plot(x, Φ.(x), label=L"\Phi", lc=:black, lw=2, legend=false, xguidefontsize=14, yguidefontsize=14)
+    xlabel!(L"x")
+    ylabel!(L"\Phi(x)")
+    savefig("plot.pdf")
+end
