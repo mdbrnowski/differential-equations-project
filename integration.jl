593542d0156b26c37bdd0a377f828da6f38e5973
Michał Dobranowski <mdbrnowski@gmail.com>
2023-12-21 13:40:53 +0100

    Minor fixes

--- integration.jl
+++ integration.jl
@@ -6,7 +6,7 @@ function gauss_legendre_quadrature(f::Function)::Real
     return dot(weights, f.(xs))
 end
 
-" Integrates f on range (a, b). Uses Gauss-Legendre quadrature with n = 3. "
+" Integrates f on range (a, b). Uses Gauss-Legendre quadrature with n = 2. "
 function integrate(f::Function, a::Real, b::Real)::Real
     g(x) = f((x + 1) / 2 * (b - a) + a)
     result = gauss_legendre_quadrature(g)
--- solution.jl
+++ solution.jl
@@ -12,36 +12,36 @@ const G::Real = 1
 
 Φ̃′(x::Real) = -1 / 3
 
-xi(i::Integer) = 3 * i / n
+x̂(i::Integer) = 3 * i / n
 
 function e(i::Integer, x::Real)::Real
-    if (xi(i - 1) < x < xi(i))
-        return (x - xi(i - 1)) / (3 / n)
-    elseif (xi(i) < x < xi(i + 1))
-        return (xi(i + 1) - x) / (3 / n)
+    if (x̂(i - 1) < x < x̂(i))
+        return (x - x̂(i - 1)) / (3 / n)
+    elseif (x̂(i) < x < x̂(i + 1))
+        return (x̂(i + 1) - x) / (3 / n)
     end
     return 0
 end
 
 function e′(i::Integer, x::Real)::Real
-    if (xi(i - 1) < x < xi(i))
+    if (x̂(i - 1) < x < x̂(i))
         return 1 / (3 / n)
-    elseif (xi(i) < x < xi(i + 1))
+    elseif (x̂(i) < x < x̂(i + 1))
         return -1 / (3 / n)
     end
     return 0
 end
 
 " returns B(e_i, e_j) "
-function B(i::Integer, j::Integer)
+function B(i::Integer, j::Integer)::Real
     i, j = minmax(i, j)
     if (j == i)
-        return -(∫(x -> e′(i, x)^2, xi(i - 1), xi(i)) +
-                 ∫(x -> e′(i, x)^2, xi(i), xi(i + 1)))
+        return -(∫(x -> e′(i, x)^2, x̂(i - 1), x̂(i)) +
+                 ∫(x -> e′(i, x)^2, x̂(i), x̂(i + 1)))
     elseif (j == i + 1)
-        return -(∫(x -> e′(i, x) * e′(j, x), xi(i - 1), xi(i)) +
-                 ∫(x -> e′(i, x) * e′(j, x), xi(i), xi(j)) +
-                 ∫(x -> e′(i, x) * e′(j, x), xi(j), xi(j + 1)))
+        return -(∫(x -> e′(i, x) * e′(j, x), x̂(i - 1), x̂(i)) +
+                 ∫(x -> e′(i, x) * e′(j, x), x̂(i), x̂(j)) +
+                 ∫(x -> e′(i, x) * e′(j, x), x̂(j), x̂(j + 1)))
     else
         return 0
     end
@@ -49,32 +49,24 @@ end
 
 " returns L(e_i) "
 function L(i::Integer)::Real
-    result = ∫(x -> Φ̃′(x) * e′(i, x), xi(i - 1), xi(i)) +
-             ∫(x -> Φ̃′(x) * e′(i, x), xi(i), xi(i + 1))
-    if (xi(i + 1) >= 1 && xi(i - 1) <= 2)
-        result += 4π * G * (∫(x -> e(i, x), xi(i - 1), xi(i)) +
-                            ∫(x -> e(i, x), xi(i), xi(i + 1)))
+    result = ∫(x -> Φ̃′(x) * e′(i, x), x̂(i - 1), x̂(i)) +
+             ∫(x -> Φ̃′(x) * e′(i, x), x̂(i), x̂(i + 1))
+    if (x̂(i + 1) >= 1 && x̂(i - 1) <= 2)
+        result += 4π * G * (∫(x -> e(i, x), x̂(i - 1), x̂(i)) +
+                            ∫(x -> e(i, x), x̂(i), x̂(i + 1)))
     end
     return result
 end
 
-A = zeros(Real, (n - 1, n - 1))
-for i in 1:n-1, j in 1:n-1
-    A[i, j] = B(i, j)
-end
-
-Y = zeros(Real, (n - 1, 1))
-for i in 1:n-1
-    Y[i, 1] = L(i)
-end
-
-weights = A\Y
+A = [B(i, j) for i in 1:n-1, j in 1:n-1]
+Y = [L(i) for i in 1:n-1]
+weights = A \ Y
 
 function Φ(x::Real)
     return dot(weights, [e(i, x) for i in 1:n-1]) + Φ̃(x)
 end
 
-x = range(0, 3, 100)
+x = range(0, 3, 1000)
 plot(x, Φ.(x), label=L"\Phi", lc=:black, lw=2, legend=false, xguidefontsize=14, yguidefontsize=14)
 xlabel!(L"x")
 ylabel!(L"\Phi(x)")
